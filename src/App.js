834127edeea3f8311b55c24a48282441b7c8f5d2
Dylan Taft <dylbronjames@gmail.com>
2019-06-17 19:27:08 -0600

    created clock and bitcoin viewer

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Clock from './components/clock/Clock';
+import Market from './components/market/Market';
+
+class App extends Component {
+  state = { showClock: false, showMarket: false }
+
+  toggleShowClock = () => {
+    const {showClock } = this.state
+    this.setState({ showClock: !showClock})
+  }
+
+  toggleShowMarket = () => {
+    const { showMarket } = this.state
+    this.setState({showMarket: !showMarket})
+  }
+
+  render() {
+    const { showClock, showMarket } = this.state
+    return (
+      <div>
+        { showClock && <Clock /> }
+        <button onClick={this.toggleShowClock}>Clock</button>
+        { showMarket && <Market /> }
+        <button onClick = {this.toggleShowMarket}>Market</button>
+      </div>
+    )
+  }
+}
+
+export default App;
